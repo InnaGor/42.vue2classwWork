bf86a479e53db5b6eeaccf4c4e19c670c953576b
inna_horbenko <inna.gorbenko@gmail.com>
2019-12-11 20:56:29 +0200

    counter

--- project2/src/App.vue
+++ project2/src/App.vue
@@ -2,16 +2,25 @@
   <div>
     <h1 v-once>{{ title }}</h1>
     <p>{{ getTitle() }}</p>
+    <a v-bind:href="url">Google</a>
+    <p v-html="link"></p>
+    <counter />
   </div>
 </template>
 
 <script>
+import Counter from "@/components/Counter";
+import { link } from "fs";
 export default {
   name: "app",
+  components: {
+    counter: Counter
+  },
   data() {
     return {
       title: "Some title",
-      url: "https://google.com"
+      url: "https://google.com",
+      link: '<a href="https://google.com">Google</a>'
     };
   },
   methods: {
---
+++ project2/src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ counter }}</h1>
+    <br />
+    <!-- <button v-on:click="IncreaseCounter">Increase counter</button> -->
+    <form action="https://google.com">
+      <input type="submit" v-on:click.prevent="IncreaseCounter" value="Increase counter" />
+    </form>
+  </div>
+</template>
+
+<script>
+// v-[directive]:[attribute].[modifier]
+export default {
+  name: "Counter",
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  methods: {
+    IncreaseCounter() {
+      this.counter++;
+    }
+  }
+};
+</script>
